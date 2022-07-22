da6e9b26f0c18e94aeafbc66c281908da93ca4bf
Manpreet <manpreetrahal27@gmail.com>
2022-07-22 02:38:13 -0600

    user functionality complete except for update and verifying emails

--- User/urls.py
+++ User/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import signup
+from .views import signup,logout,login
 
 urlpatterns = [
-    path('signup/',signup, name='user_signup')
+    path('signup/',signup, name='user_signup'),
+    path('login/',login,name='user_login'),
+    path('logout/',logout,name='user_logout')
 ]
--- User/views.py
+++ User/views.py
@@ -4,12 +4,14 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_GET as get
 from django.views.decorators.http import require_POST as post
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login as user_login,logout as user_logout
 
 # signup
 @post
 @csrf_exempt
 def signup(request):
-    # can be refactored to a function
+    # can be refactored to a middleware
     try:
         # get the request body
         user_details = json.loads(request.body)
@@ -20,7 +22,40 @@ def signup(request):
         return JsonResponse({'msg':'missing user details'},
                             status=400)
 
-    return JsonResponse({'msg':user_details})
+    # create user
+    try:
+        user = User.objects.create_user(username,email,password)
+        user.save()
+    except Exception as e:
+        print(e)
+        return JsonResponse({'msg':'error occured'})
+
+    return JsonResponse({'msg':'user created'})
 
 # login
+@post
+@csrf_exempt
+def login(request):
+    try:
+        # get the request body
+        user_details = json.loads(request.body)
+        username = user_details['username']
+        password = user_details['password']
+    except:
+        return JsonResponse({'msg':'missing user details'},
+                            status=400)
+
+    user = authenticate(request,username=username,password=password)
+    if user is not None:
+        user_login(request,user)
+        return JsonResponse({'msg':'logged in'})
+    else:
+        return JsonResponse({'msg':"invalid creds"},status=420)
+
+
 # logout
+@get
+@csrf_exempt
+def logout(request):
+    user_logout(request)
+    return JsonResponse({'msg':'logged out'})
