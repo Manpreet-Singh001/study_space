caaacdba3b4cdfa44d46e058f1d6ffa4e236a429
Manpreet <manpreetrahal27@gmail.com>
2022-07-22 02:55:46 -0600

    created the session models

--- study_session/models.py
+++ study_session/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class StudySession(models.Model):
+    start_time = models.DateTimeField(default=now)
+    end_time = models.DateTimeField(null=True)
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=30)
+    session_id = models.ForeignKey(StudySession,on_delete=models.CASCADE)
+
+
+class Note(models.Model):
+    note = models.TextField()
+    topic_id = models.ForeignKey(Topic,on_delete=models.CASCADE)
+
